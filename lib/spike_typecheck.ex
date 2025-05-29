a3ecfae5bf2db712ad3fc0ea1baf806ccf896d26
Radek Szymczyszyn <radek.szymczyszyn@v7labs.com>
2025-05-29 09:43:30 +0200

    Save project structure
      mix new spike_typecheck

---
+++ lib/spike_typecheck.ex
@@ -0,0 +1,18 @@
+defmodule SpikeTypecheck do
+  @moduledoc """
+  Documentation for `SpikeTypecheck`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SpikeTypecheck.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/spike_typecheck_test.exs
@@ -0,0 +1,8 @@
+defmodule SpikeTypecheckTest do
+  use ExUnit.Case
+  doctest SpikeTypecheck
+
+  test "greets the world" do
+    assert SpikeTypecheck.hello() == :world
+  end
+end
